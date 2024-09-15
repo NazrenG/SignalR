e3675256579218db4f1430eb59187e5f9ab0bdce
Nəzrin Quliyeva <140939317+NazrenG@users.noreply.github.com>
2024-09-15 21:22:02 +0400

    Add project files.

---
+++ SignalR/Controllers/OfferController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.Helpers;
+
+namespace SignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        [HttpGet]
+        public double Get()
+        {
+            return FileHelper.Read();
+        }
+
+        [HttpGet("Increase")]
+        public void Increase(double data)
+        {
+            var result = FileHelper.Read() + data;
+            FileHelper.Write(result);
+        }
+    }
+}
---
+++ SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Helpers;
+
+namespace SignalR.Hubs
+{
+    public class MessageHub:Hub
+    {
+        //hazir metod-qosulduqda tetiklenir
+        public override async Task OnConnectedAsync()
+        {
+            //hamiya gonderir
+            //ReceiveConnectedInfo=> bu metodu cagiririq heryerde
+            await Clients.Others.SendAsync("ReceiveConnectInfo", "User connected");
+        }
+
+        //hubdan cixdiqda isleyir
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.Others.SendAsync("ReceiveDisConnectedInfo", "User Disconnected");
+        }
+
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",message,FileHelper.Read());
+        }
+
+       
+
+    }
+}
